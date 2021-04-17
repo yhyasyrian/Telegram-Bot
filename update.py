0b653116ac9a8651bfe11fe425de0e6635fc8363
Yhya syrian <77106078+yhyasyrian@users.noreply.github.com>
2021-04-17 23:24:46 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,17 +1,17 @@
 import requests
-Token = input('Enter Your Token Bot')
+Token = input('Enter Your Token Bot:')
 API_URL = 'https://api.telegram.org/bot'+Token+'/'
 def getupdate(offset):
   update = requests.get(API_URL+'getupdates?offset='+str(offset)+'&limit=1')
   return update.json()['result'][0]
-def run(update):
- print(update)
+def sendmessage(chat_id,text):
+	url = requests.get(API_URL+'sendmessage?chat_id='+str(chat_id)+'&text='+str(text))
+	return url.json()
 update_id = 0
 while True:
-  try:
-	  update = getupdate(update_id+1)
-	  run(update)
-	  update_id = int(update['update_id'])
-  except:
-    continue
-
+ try:
+   update = getupdate(update_id+1)
+   print(update)
+   update_id = int(update['update_id'])
+ except:
+  continue
